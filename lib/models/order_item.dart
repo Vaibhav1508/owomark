77153ea50a158b47cce51903ffacf26556ddc1f7
Vaibhav1508 <vaibhavmehta974@gmail.com>
2019-12-17 15:28:06 +0530

    Add files via upload

--- lib/models/order_item.dart
+++ lib/models/order_item.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/cupertino.dart';
 
+class OrderItems {
+  String id = "";
+  String name = "";
+  String imageUrl = "";
+  String status = "";
+  String time = "";
+  String price = "";
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'order_no': this.name,
+      'img': this.imageUrl,
+      'delivery': this.status,
+      'date': this.time,
+      'amount': this.price,
+    };
+  }
+
+  factory OrderItems.fromMap(Map<String, dynamic> map) {
+    return new OrderItems(
+      id: map['id'] as String,
+      name: map['order_no'] as String,
+      imageUrl: map['img'] as String,
+      status: map['delivery'] as String,
+      time: map['date'] as String,
+      price: map['amount'] as String,
+    );
+  }
+
+  OrderItems({
+    @required this.id,
+    @required this.name,
+    @required this.imageUrl,
+    @required this.status,
+    @required this.time,
+    @required this.price,
+  });
+}
--- lib/models/payment_item.dart
+++ lib/models/payment_item.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/cupertino.dart';
 
+class PaymentItem {
+  String cod = "";
+  String wallet = "";
+  String balance = "";
+
+  Map<String, dynamic> toMap() {
+    return {
+      'cod': this.cod,
+      'wallet': this.wallet,
+      'balance': this.balance,
+    };
+  }
+
+  factory PaymentItem.fromMap(Map<String, dynamic> map) {
+    return new PaymentItem(
+      wallet: map['wallet'] as String,
+      cod: map['cod'] as String,
+      balance: map['balance'] as String,
+    );
+  }
+
+  PaymentItem({
+    @required this.cod,
+    @required this.wallet,
+    @required this.balance,
+  });
+}
